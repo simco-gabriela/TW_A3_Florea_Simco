5f37870b0b32630ed8b0eafaa3809d8eb8533192
SoapJuice <cosminflorin_florea@yahoo.ro>
2024-06-22 22:43:21 +0300

    navbar and footer separeted

---
+++ Proiect/js/navbar_and_footer.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function setupHamburgerMenu() {
+        const hamburger = document.querySelector('.hamburger');
+        const menu = document.querySelector('.menu');
+
+        if (hamburger && menu) {
+            hamburger.addEventListener('click', function () {
+                menu.classList.toggle('active');
+            });
+        }
+    }
+
+    function loadHTML(file, elementID, callback) {
+        fetch(file)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById(elementID).innerHTML = data;
+                if (callback) callback();
+            });
+    }
+
+    function initialize() {
+        loadHTML('footer.html', 'footer', setupHamburgerMenu);
+        loadHTML('navbar.html', 'navbar', setupHamburgerMenu);
+    }
+    initialize();
+});
+
